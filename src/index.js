7f5a92142a681f346adde224ac26ac16591562cb
Rexxar1919 <vladimir.popov1998@mail.ru>
2021-08-30 11:50:54 +0300

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 
 // You should implement your task here.
 
+
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == null || matrix.length == 0) {
+    return [];
+  }
+  let bebra = [], bibip = 0;
+  for (let biba = 0; biba < matrix.length; biba++) {
+    for (let boba = 0; boba < matrix[biba].length; boba++) {
+      if (biba%2 == 0) {
+        bebra[bibip] = matrix[biba][boba];
+        bibip++;
+      } else {
+        bebra[bibip] = matrix[biba][matrix[biba].length - 1 - boba];
+        bibip++;
+      }
+      
+    }
+    
+  }
+  return bebra;
 }
